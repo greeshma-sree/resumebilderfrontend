9db1cb76c3641c750396e34bb6abaa1970851a40
greeshma-sree <greeshmajayan17@gmail.com>
2025-10-15 19:49:03 +0530

    update url

--- src/service/serverURL.js
+++ src/service/serverURL.js
@@ -1,4 +1,4 @@
 // const BASEURL="http://localhost:3000"
 
-const BASEURL="https://resumebilder-server-2.onrender.com/"
+const BASEURL="https://resumebilder-server-2.onrender.com"
 export default BASEURL
